19f2696c887f675a5868954a514ab12adbf2f925
Zaneeyboy <40801075+Zaneeyboy@users.noreply.github.com>
2022-07-20 12:23:29 -0400

    Added Vendor Process DOcs

--- theme.config.js
+++ theme.config.js
@@ -1,12 +1,14 @@
 export default {
-  github: 'https://github.com/shuding/nextra',
-  docsRepositoryBase: 'https://github.com/shuding/nextra/blob/master',
-  titleSuffix: ' – Nextra',
+  github: 'https://github.com/Zaneeyboy/EXIM-Procurement',
+  docsRepositoryBase:
+    'https://github.com/Zaneeyboy/EXIM-Procurement/tree/main/pages',
+
+  titleSuffix: 'EXIM',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+      <span className="mr-2 font-extrabold hidden md:inline">EXIM BANK</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Next.js Static Site Generator
+        Procurement Process
       </span>
     </>
   ),
@@ -16,25 +18,30 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the Next.js site builder" />
-      <meta name="og:description" content="Nextra: the Next.js site builder" />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="twitter:site:domain" content="nextra.vercel.app" />
-      <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
-      <meta name="og:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="apple-mobile-web-app-title" content="Nextra" />
+      <meta property="og:title" content="EXIM BANK : Procurement Process" />
+      <title>EXIM Procurement Docs</title>
+      <meta
+        name="description"
+        content="EXIM BANK : Procurement Process Documentation"
+      />
+      <meta
+        name="og:description"
+        content="EXIM BANK : Procurement Process Documentation"
+      />
+      <meta
+        name="apple-mobile-web-app-title"
+        content="EXIM BANK Procurement Process"
+      />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="/apple-icon-180x180.png"
+        href="/favicon-180x180.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="192x192"
-        href="/android-icon-192x192.png"
+        href="/favicon-192x192.png"
       />
       <link
         rel="icon"
@@ -54,7 +61,6 @@ export default {
         sizes="16x16"
         href="/favicon-16x16.png"
       />
-      <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
     </>
   ),
   search: true,
@@ -62,6 +68,8 @@ export default {
   nextLinks: true,
   footer: true,
   footerEditLink: 'Edit this page on GitHub',
-  footerText: <>MIT {new Date().getFullYear()} © Nextra.</>,
+  footerText: (
+    <>{new Date().getFullYear()} © Export Import Bank of Trinidad and Tobago</>
+  ),
   unstable_faviconGlyph: '👋',
 }
